4b37f354eefa07035ad5932eefa401b5bc89c472
Jungwon Goda <koag132@gmail.com>
2020-12-28 22:59:29 -0800

    Added food truck markers on the map

--- src/main/resources/static/js/app.jsx
+++ src/main/resources/static/js/app.jsx
@@ -7,27 +7,28 @@ const Link = ReactRouterDOM.Link;
 const Redirect = ReactRouterDOM.Redirect;
 
 function App() {
-
+    console.log('app render');
     return (
         <Home />
     );
 }
 
-function Home(){
-    const [data, setData] = React.useState();
+function Home(props){
+    console.log('home render');
+    const [data, setData] = React.useState([]);
 
     return( 
         <div className="whole-container">
             <div>
                 <label>Food Hunters</label>
-                <SearchBar setData={setData}/>
+                <SearchBar data={data} setData={setData} />
             </div>
             <div id="main-container">
                 <div id="list-container">
-
+                    {/* <ListContainer data={data} /> */}
                 </div>
                 <div id="map-div-container">
-                    <MapContainer />
+                    <MapContainer data={data} />
                 </div>
                 <div id="info-container">
 
@@ -41,6 +42,7 @@ function Home(){
 }
 
 function SearchBar(props){
+    console.log('search bar render');
     const [isError, setIsError] = React.useState(false);
     const [searchInput, setSearchInput] = React.useState('');
     console.log(isError, searchInput);
@@ -48,12 +50,30 @@ function SearchBar(props){
     const handleSubmit = async event =>{
         event.preventDefault();
         console.log('event handler is working');
-        //get the search input from the form
-        console.log(isError, searchInput);
+        console.log(isError, searchInput, props.data);
 
         //get request to /search in the server
         $.get('/search', (response)=>{
-            console.log(response);
+            console.log(response); // a list of objects
+            //filter the data based on the searchInput
+            const searchData = [];
+            let foodItems = '';
+            for(const object of response){
+                if(object['fooditems']){
+                    //convert all the caracters in the string into lowercase
+                    foodItems = object['fooditems'].toLowerCase();
+                    //if the foodItems includes the searchInput(if it's true)
+                    if(foodItems.includes(searchInput)){
+                        //push the object into the search data array
+                        searchData.push(object);
+                    }
+
+                }
+            }
+
+            //set the data to the filtered data from the server
+            props.setData(searchData); 
+
         }).fail(()=>{
             setIsError(true);
             alert('Fail to retrieve the data');
@@ -65,7 +85,7 @@ function SearchBar(props){
             <form onSubmit={handleSubmit}>
                 <input type="text" onChange={(e)=>{
                     e.preventDefault();
-                    setSearchInput(e.target.value);
+                    setSearchInput(e.target.value.toLowerCase());
                 }} name="search-input" id="search-input" placeholder="What are you craving for?"></input>
                 <input type="submit" value="search" id="search-btn"></input>
             </form>
@@ -112,12 +132,14 @@ function MapComponent(props){
 }
 
 function MapContainer(props){
+    console.log('map container render')
     //map and map options
     const [map, setMap] = React.useState();
     const [options, setOptions] = React.useState({
         center: {lat: 37.77397, lng: -122.431297},
         zoom: 10
     });
+    const [markers, setMarkers] = React.useState([]);
 
     const mapDimensions = {
         width: '100%',
@@ -126,6 +148,46 @@ function MapContainer(props){
 
     if (map){
         console.log('hi, there is a map');
+        console.log(markers);
+        
+        if(markers.length !== 0){
+            console.log('is empty');
+            //setMarkers([]);
+        }
+
+        if(props.data.length !== 0 && markers.length === 0){
+            console.log(props.data);
+            let curMarkers = [];
+            //create markers with an Info container
+            for(const truck of props.data){
+                const truckInfo = new google.maps.InfoWindow();
+                const truckInfoContent = (`
+                    <div class="window-content">
+                        <ul class="truck-info">
+                            <li><b>Address: </b>${truck['address']}</li>
+                            <li><b>Name: </b>${truck['applicant']}</li>
+                            <li><b>Food Items: </b>${truck['fooditems']}</li>
+                        </ul>
+                    </div>
+                `);
+
+                const truckMarker = new window.google.maps.Marker({
+                    position: {lat: parseFloat(truck['latitude']), lng: parseFloat(truck['longitude'])},
+                    title: `${truck['applicant']}`,
+                    map: map
+                });
+
+                curMarkers.push(truckMarker);
+                
+                truckMarker.addListener('click', ()=>{
+                    truckInfo.close();
+                    truckInfo.setContent(truckInfoContent);
+                    truckInfo.open(map, truckMarker);
+                });
+            }
+            console.log(curMarkers);
+            setMarkers(curMarkers);
+        } 
     }
 
     const MainMap = React.useCallback(
